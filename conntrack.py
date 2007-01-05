a5ba7c646039338519fe50976d4b235a0ac22304
haypo <haypo@3549fb6e-961a-0410-b3b8-ef7231af4ca6>
2007-01-05 10:15:23 +0000

    Add command line option to choose between plaintext or XML output
    git-svn-id: http://software.inl.fr/svn/mirror/edenwall/pynetfilter_conntrack/trunk@3267 3549fb6e-961a-0410-b3b8-ef7231af4ca6

--- conntrack.py
+++ conntrack.py
@@ -1,18 +1,32 @@
 #!/usr/bin/env python
 
-from pynetfilter_conntrack import Conntrack
+from pynetfilter_conntrack import Conntrack, NFCT_O_DEFAULT, NFCT_O_XML
 from socket import AF_INET
+import sys
+
+OUTPUT_FORMAT = {
+    "list": NFCT_O_DEFAULT,
+    "xml": NFCT_O_XML,
+}
 
 def main():
+    if len(sys.argv) != 2 or sys.argv[1] not in OUTPUT_FORMAT:
+        print >>sys.stderr, "usage: %s command" % sys.argv[0]
+        print >>sys.stderr
+        print >>sys.stderr, "command: list or xml"
+        sys.exit(1)
+    mode = sys.argv[1]
+    output = OUTPUT_FORMAT[mode]
+
+    if mode == "xml":
+        print '<?xml version="1.0" encoding="ASCII"?>'
+        print '<flows>'
     nf = Conntrack()
     table = nf.dump_table(AF_INET)
     for entry in table:
-        entry.mark = 42
-        entry.update()
-
-    table = nf.dump_table(AF_INET)
-    for entry in table:
-        print entry
+        print entry.format(output)
+    if mode == "xml":
+        print '</flows>'
 
 if __name__ == "__main__":
     main()
